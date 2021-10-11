fafbf9f68f4d23f5ab36efb3b1c1d88ec16c3066
Satyen Singh <srs833@nyu.edu>
2021-10-11 11:23:40 -0400

    changed admin

--- food_redistribution/accounts/admin.py
+++ food_redistribution/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Restaurant, FoodRedistributor, Individual
 
-# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(FoodRedistributor)
+admin.site.register(Individual)
